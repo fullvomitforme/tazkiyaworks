25f6b92a096ae270163d47f68881e7ad18107210
Tazkiya Anafsi Al Mujahid <tazkiyadigitalarchive@gmail.com>
2024-01-06 21:46:26 +0700

    📦 NEW: Refactor AboutPage component and add work section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,14 @@
 import RunningDesign from '@/src/components/ui/running-design/RunningDesign';
+import { buttonVariants } from '@/src/components/ui/button';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const AboutPage = () => {
 	return (
 		<section>
 			<div className='container'>
 				<div className='space-y-5'>
-					<h2 className='font-medium font-editorialNew text-lg text-orange-400'>
-						gtkm
-					</h2>
+					<h2 className='font-medium text-lg text-orange-400'>GTKM</h2>
 					<p className='font-medium text-xl leading-relaxed'>
 						I&apos;m Tazkiya Mujahid (pronounced &quot;TAZ-kee-yah
 						Moo-jahid&quot;), a Jakarta-based Junior Quality Assurance
@@ -33,6 +34,43 @@ const AboutPage = () => {
 			</div>
 			<div className='my-10'>
 				<RunningDesign />
+			</div>
+			<div className='container space-y-5'>
+				<div className='flex flex-row items-center justify-between'>
+					<p className='font-medium text-lg text-orange-400'>WORK</p>
+					<Link className={buttonVariants({ variant: 'default' })} href={'/'}>
+						DOWNLOAD RESUME
+					</Link>
+				</div>
+				<div>
+					<div className='border-2 border-orange-400 rounded-lg'>
+						<div className='flex flex-row p-5 items-center space-x-5'>
+							<Image
+								src={'/work/looking.png'}
+								width={120}
+								height={120}
+								alt='looking for you'
+							/>
+							<div>
+								<p className='font-medium mb-4'>??? (this could be you!)</p>
+								<p>
+									I&apos;m on the lookout for exciting remote freelance
+									opportunities as a dedicated Quality Assurance Tester. If you
+									need someone who values excellence and delivers results,
+									let&apos;s connect! Reach out at{' '}
+									<Link
+										href={'mailto:tazkiyadigitalarchive@gmail.com'}
+										className='font-medium'
+									>
+										tazkiyadigitalarchive@gmail.com
+									</Link>
+									, and let&apos;s discuss how I can contribute to your
+									projects. 🚀
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
