f65072067aede20dbbda0d26de0051a16eba7287
Tazkiya Anafsi Al Mujahid <tazkiyadigitalarchive@gmail.com>
2024-01-11 17:28:13 +0700

    👌 IMPROVE: Add image to Work page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,36 @@
 import RunningText from '@/src/components/ui/running-text/RunningText';
 import MainCards from '../components/ui/cards/MainCards';
+import Image from 'next/image';
 
 export default function Work() {
 	return (
 		<main className='flex flex-col justify-center items-center space-y-20'>
+			<div>
+				<Image
+					src={'/logos/tazkiya_works.png'}
+					alt='fuck around and find out'
+					width={1000}
+					height={500}
+					className='m-10'
+				></Image>
+			</div>
 			<div className='container flex justify-center items-center'>
 				<h1 className='font-normal text-3xl leading-relaxed'>
 					I&apos;m Tazkiya Mujahid, a craftsman of{' '}
-					<span className='text-red-400 font-editorialNew'>
+					<span className='text-highlight font-editorialNew'>
 						code, interfaces, and digital landscapes.
 					</span>{' '}
 					As a Junior Quality Assurance enthusiast hailing from Jakarta,
 					I&apos;ve honed my skills in{' '}
-					<span className='text-red-400 font-editorialNew'>
+					<span className='text-highlight font-editorialNew'>
 						testing automation, documentation, and more.
 					</span>{' '}
 					With a focus on precision and quality, I&apos;ve delved into{' '}
-					<span className='text-red-400 font-editorialNew'>
+					<span className='text-highlight font-editorialNew'>
 						TypeScript, Node.js, Playwright, and React.
 					</span>{' '}
 					In the realm of design, I weave my creativity into{' '}
-					<span className='text-red-400 font-editorialNew'>
+					<span className='text-highlight font-editorialNew'>
 						UI/UX aesthetics,
 					</span>{' '}
 					adding a splash of graphic design finesse to my tech repertoire.
