9d7fec40b5f1b2284eb3b5b3e3a4a0703a719684
Tazkiya Anafsi Al Mujahid <tazkiyadigitalarchive@gmail.com>
2024-01-11 17:28:34 +0700

    👌 IMPROVE: Update Navbar styling and add dancing link

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,10 +4,10 @@ import Routes from '@/src/components/navbar/routes/Routes';
 
 const Navbar = () => {
 	return (
-		<nav className='container flex flex-row justify-center items-center space-x-5 my-10'>
-			<Link href={'/'} className='p-2'>
-				<TazkiyaWorksLogo />
-			</Link>
+		<nav className='container flex flex-row justify-center items-center space-x-5 my-20'>
+			<div className='text-[30px]'>
+				<Link href={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}>🕺🏻💃🏻</Link>
+			</div>
 			<div className='p-2'>
 				<Routes />
 			</div>
