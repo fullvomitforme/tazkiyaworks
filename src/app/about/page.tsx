f3dc44f75cf11faffdc347c3a0f29bffedaffdc4
Tazkiya Anafsi Al Mujahid <tazkiyadigitalarchive@gmail.com>
2024-01-17 17:20:24 +0700

    👌 IMPROVE: Update UI in ExperienceCard and RunningDesign components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,87 +4,86 @@ import Link from 'next/link';
 import Image from 'next/image';
 import ExperienceCard from '@/src/components/about/experience-card/ExperienceCard';
 import WorkinProgress from '@/public/logos/workinprogress.png';
+import Think from '@/public/acc/think.png';
 
 const AboutPage = () => {
   return (
-    <section className='container mx-auto p-4'>
-      <div className='flex items-center justify-center'>
-        <Image src={WorkinProgress} alt='Work in Progress' sizes='100vw' />
-      </div>
-      {/* <div className='container'>
-        <div className='space-y-5'>
-          <h2 className='text-lg font-medium text-orange-400'>GTKM</h2>
-          <p className='text-xl font-medium leading-relaxed'>
-            I&apos;m Tazkiya Mujahid (pronounced &quot;TAZ-kee-yah
-            Moo-jahid&quot;), a Jakarta-based Junior Quality Assurance
-            enthusiast and an undergraduate student at Mercu Buana University,
-            majoring in Information System. I have a flair for crafting seamless
-            digital experiences. In the tech realm, I specialize in testing
-            automation using tools like TypeScript, Node.js, and Playwright,
-            prioritizing precision and quality.
-          </p>
-          <p className='text-xl font-medium leading-relaxed'>
-            Beyond code, I infuse creativity into UI/UX projects, adding a touch
-            of graphic design finesse. I prefer function declarations and
-            descriptive TypeScript types and thrive on creative problem-solving.
-          </p>
-          <p className='text-xl font-medium leading-relaxed'>
-            I&apos;m deeply passionate about design—I&apos;ve had the joy of
-            crafting captivating visuals and user experiences during my time at
-            Heulaulastudios. Design isn&apos;t just a skill for me; it&apos;s a
-            love affair. I find immense joy in the creative process, infusing
-            every project with a touch of aesthetic finesse.
-          </p>
-        </div>
+    <section className='m-4 p-4'>
+      <div className='space-y-2'>
+        <h2 className='text-lg font-medium leading-relaxed tracking-wider text-highlight'>
+          GTKM
+        </h2>
+        <p className='text-sm font-normal leading-relaxed'>
+          I&apos;m Tazkiya Mujahid, a Jakarta-based Junior Quality Assurance
+          enthusiast and an undergraduate student at Mercu Buana University,
+          majoring in Information System. I have a flair for crafting seamless
+          digital experiences. In the tech realm, I specialize in testing
+          automation using tools like TypeScript, Node.js, and Playwright,
+          prioritizing precision and quality.
+        </p>
+        <p className='text-sm font-normal leading-relaxed'>
+          Beyond code, I infuse creativity into UI/UX projects, adding a touch
+          of graphic design finesse. I prefer function declarations and
+          descriptive TypeScript types and thrive on creative problem-solving.
+        </p>
+        <p className='text-sm font-normal leading-relaxed'>
+          I&apos;m deeply passionate about design—I&apos;ve had the joy of
+          crafting captivating visuals and user experiences during my time at
+          Heulaulastudios. Design isn&apos;t just a skill for me; it&apos;s a
+          love affair. I find immense joy in the creative process, infusing
+          every project with a touch of aesthetic finesse.
+        </p>
       </div>
       <div className='my-10'>
         <RunningDesign />
       </div>
-      <div className='container min-h-[600px] space-y-5'>
+      <div className='mb-5 flex flex-col space-y-5'>
         <div className='flex flex-row items-center justify-between'>
-          <p className='text-lg font-medium text-orange-400'>WORK</p>
+          <h2 className='text-lg font-medium leading-relaxed tracking-wider text-highlight'>
+            WORK
+          </h2>
           <Link
-            className={buttonVariants({ variant: 'default' })}
             href={
               'https://tazkiyamujahid.notion.site/Tazkiya-Anafsi-Al-Mujahid-Resume-da37e36e6635439da6f41a64ffa02e00?pvs=4'
             }
+            className='hover:bg-highlight/20 rounded-lg bg-highlight p-2 font-medium text-white'
           >
-            DOWNLOAD RESUME
+            Download Resume
           </Link>
         </div>
-        <div className='flex flex-col items-center justify-center gap-5'>
-          <div className='rounded-lg border-2 border-orange-400'>
-            <div className='flex flex-row items-center space-x-5 p-5'>
-              <Image
-                src={'/work/looking.png'}
-                width={120}
-                height={120}
-                alt='looking for you'
-              />
-              <div>
-                <p className='mb-4 font-medium'>??? (this could be you!)</p>
-                <p>
-                  I&apos;m on the lookout for exciting remote freelance
-                  opportunities as a dedicated Quality Assurance Tester. If you
-                  need someone who values excellence and delivers results,
-                  let&apos;s connect! Reach out at{' '}
-                  <Link
-                    href={'mailto:tazkiyadigitalarchive@gmail.com'}
-                    className='font-medium'
-                  >
-                    tazkiyadigitalarchive@gmail.com
-                  </Link>
-                  , and let&apos;s discuss how I can contribute to your
-                  projects. 🚀
-                </p>
-              </div>
+        <div className='flex cursor-help flex-col items-center justify-center'>
+          <div className='p- mb-2 flex flex-col items-center justify-between rounded-lg border border-highlight p-4'>
+            <Image
+              src={Think}
+              alt='think'
+              style={{ width: '110px', height: '110px' }}
+              className='float-left mb-2 flex cursor-pointer rounded-lg'
+            />
+            <div>
+              <h2 className='mb-2 text-lg font-medium'>
+                ??? (this could be you!)
+              </h2>
+              <p className='text-justify text-sm font-normal'>
+                I&apos;m on the lookout for exciting remote freelance
+                opportunities as a dedicated Quality Assurance Tester. If you
+                need someone who values excellence and delivers results,
+                let&apos;s connect! Reach out at{' '}
+                <Link
+                  href={'mailto:tazkiyadigitalarchive@gmail.com'}
+                  className='font-medium text-highlight'
+                >
+                  tazkiyadigitalarchive@gmail.com
+                </Link>
+                , and let&apos;s discuss how I can contribute to your projects.
+                🚀
+              </p>
             </div>
           </div>
         </div>
-        <div className='pb-10'>
-          <ExperienceCard />
-        </div>
-      </div> */}
+      </div>
+      <div>
+        <ExperienceCard />
+      </div>
     </section>
   );
 };
--- src/components/ui/running-design/RunningDesign.tsx
+++ src/components/ui/running-design/RunningDesign.tsx
@@ -1,67 +1,69 @@
 import Marquee from 'react-fast-marquee';
 import Image from 'next/image';
+import hguWorker from '@/public/running-design/hgu-worker.jpg';
+import mitskiFanart from '@/public/running-design/mitski-fanart.jpg';
+import novaNugas from '@/public/running-design/nova-nugas.jpg';
+import novaUas from '@/public/running-design/nova-uas.jpg';
+import playlist from '@/public/running-design/playlist.jpg';
+import tbcDonation from '@/public/running-design/tbc-donation.jpg';
 
 const RunningDesign = () => {
   return (
-    <Marquee speed={100}>
-      <div className='flex flex-row items-center justify-around'>
-        {/* Duplicate some images at the end to maintain spacing during looping */}
+    <Marquee speed={100} autoFill>
+      <div className='flex flex-row items-center'>
         <Image
-          src={'/running-design/hgu-worker.jpg'}
+          src={hguWorker}
+          sizes='100vw'
+          style={{ width: '250px', height: 'auto' }}
           alt='hgu-worker'
-          width={250}
-          height={250}
-          className='mr-[100px] rounded-lg'
+          className=' m-5 flex cursor-pointer rounded-lg'
+          placeholder='blur'
+          priority
         />
         <Image
-          src={'/running-design/mitski-fanart.jpg'}
+          src={mitskiFanart}
           alt='mitski-fanart'
-          width={250}
-          height={250}
-          className='mr-[100px] rounded-lg'
+          sizes='100vw'
+          style={{ width: '250px', height: 'auto' }}
+          className='m-5 flex cursor-pointer rounded-lg'
+          placeholder='blur'
+          priority
         />
         <Image
-          src={'/running-design/nova-nugas.jpg'}
+          src={novaNugas}
           alt='nova-nugas'
-          width={250}
-          height={250}
-          className='mr-[100px] rounded-lg'
+          sizes='100vw'
+          style={{ width: '250px', height: 'auto' }}
+          className='m-5 flex cursor-pointer rounded-lg'
+          placeholder='blur'
+          priority
         />
         <Image
-          src={'/running-design/nova-uas.jpg'}
+          src={novaUas}
           alt='nova-uas'
-          width={250}
-          height={250}
-          className='mr-[100px] rounded-lg'
+          sizes='100vw'
+          style={{ width: '250px', height: 'auto' }}
+          className='m-5 flex cursor-pointer rounded-lg'
+          placeholder='blur'
+          priority
         />
         <Image
-          src={'/running-design/playlist.jpg'}
+          src={playlist}
           alt='playlist'
-          width={250}
-          height={250}
-          className='mr-[100px] rounded-lg'
+          sizes='100vw'
+          style={{ width: '250px', height: 'auto' }}
+          className='m-5 flex cursor-pointer rounded-lg'
+          placeholder='blur'
+          priority
         />
         <Image
-          src={'/running-design/tbc-donation.jpg'}
+          src={tbcDonation}
           alt='tbc-donation'
-          width={250}
-          height={250}
-          className='mr-[100px] rounded-lg'
-        />
-        {/* Duplicate some images at the beginning to maintain spacing during looping */}
-        <Image
-          src={'/running-design/hgu-worker.jpg'}
-          alt='hgu-worker'
-          width={250}
-          height={250}
-          className='mr-[250px] rounded-lg'
-        />
-        <Image
-          src={'/running-design/mitski-fanart.jpg'}
-          alt='mitski-fanart'
-          width={250}
-          height={250}
-          className='mr-[250px] rounded-lg'
+          sizes='100vw'
+          style={{ width: '250px', height: 'auto' }}
+          className='m-5 flex cursor-pointer rounded-lg'
+          placeholder='blur'
+          priority
         />
       </div>
     </Marquee>
