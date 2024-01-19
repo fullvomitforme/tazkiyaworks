811a8f79adc0156b8fda97670824b14708d8dd5c
Tazkiya Anafsi Al Mujahid <tazkiyadigitalarchive@gmail.com>
2024-01-19 17:15:26 +0700

    📦 NEW: Add ProjectGallery component

---
+++ src/components/project/ProjectGallery.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { ImageHeulaulastudios } from '@/src/lib/data';
+import Image from 'next/image';
+import { useTransform, useScroll, motion } from 'framer-motion';
+import { useEffect, useRef } from 'react';
+import Lenis from '@studio-freight/lenis';
+import useDimension from '@/src/components/project/useDimension';
+
+const ProjectGallery = () => {
+  const container = useRef(null);
+
+  const { height } = useDimension();
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start end', 'end start'],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  return (
+    <main>
+      <div ref={container} className='gallery rounded-lg align-middle'>
+        <Column
+          images={[
+            ImageHeulaulastudios[0],
+            ImageHeulaulastudios[1],
+            ImageHeulaulastudios[2],
+          ]}
+          y={y}
+        />
+        <Column
+          images={[
+            ImageHeulaulastudios[3],
+            ImageHeulaulastudios[4],
+            ImageHeulaulastudios[5],
+          ]}
+          y={y2}
+        />
+        <Column
+          images={[
+            ImageHeulaulastudios[6],
+            ImageHeulaulastudios[7],
+            ImageHeulaulastudios[8],
+          ]}
+          y={y3}
+        />
+        <Column
+          images={[
+            ImageHeulaulastudios[9],
+            ImageHeulaulastudios[10],
+            ImageHeulaulastudios[11],
+          ]}
+          y={y4}
+        />
+      </div>
+    </main>
+  );
+};
+
+export default ProjectGallery;
+
+const Column = ({ images, y = 0 }: any) => {
+  return (
+    <motion.div style={{ y }} className='column'>
+      {images.map((src: any, index: any) => {
+        return (
+          <div key={index} className='imageContainer'>
+            <Image
+              src={`/work/heulaulastudios/${src}`}
+              fill
+              alt='image'
+              className='rounded-lg object-cover'
+            />
+          </div>
+        );
+      })}
+    </motion.div>
+  );
+};
