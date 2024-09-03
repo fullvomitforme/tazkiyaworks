dbbb8c758d7b8360cddf0eedc215648e91a832b0
fullvomitforme <tazkiyadigitalarchive@gmail.com>
2024-09-03 20:25:55 +0700

    chore: Add LoadingScreen component and update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import SplashScreen from '@/src/components/ui/SplashScreen';
+import LoadingScreen from '../components/ui/LoadingScreen';
 
 export default function loading() {
-  return <SplashScreen />;
+  return <LoadingScreen />;
 }
