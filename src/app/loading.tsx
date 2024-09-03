fac1f30a1b6229d62159d72e33d6b222ce327790
fullvomitforme <tazkiyadigitalarchive@gmail.com>
2024-09-03 20:36:07 +0700

    feat: Remove SplashScreen import and update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import SplashScreen from '@/src/components/ui/SplashScreen';
 import LoadingScreen from '../components/ui/LoadingScreen';
 
 export default function loading() {
