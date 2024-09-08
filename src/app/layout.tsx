541345b9232d59e37f0821e57781ee6a080a4b72
fullvomitforme <tazkiyadigitalarchive@gmail.com>
2024-09-08 14:52:49 +0700

    Refactor: Update body background styling in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,14 +37,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body
-        className={cn(
-          'overflow-hidden',
-          dmSans.variable,
-          instrumentSerif.variable
-        )}
-      >
-        <div className='absolute inset-0 -z-10 w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] bg-repeat'></div>
+      <body className={cn('', dmSans.variable, instrumentSerif.variable)}>
+        <div className='fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] bg-repeat'></div>
+
         <Cursor />
         <SplashScreen />
         <Navbar />
