ff2627b84280059e52349d6baf47152af8bc4c05
fullvomitforme <tazkiyadigitalarchive@gmail.com>
2024-09-15 14:18:17 +0700

    Refactor: Add next-umami package and UmamiProvider to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { cn } from '@/src/lib/utils';
 import Cursor from '@/src/components/ui/sticky-cursor';
 import SplashScreen from '../components/ui/SplashScreen'; 
 import SmoothScrolling from '../components/ui/SmoothScrolling';
+import UmamiProvider from 'next-umami';
 
 const dmSans = DM_Sans({
   subsets: ['latin'],
@@ -22,9 +23,8 @@ const instrumentSerif = Instrument_Serif({
 });
 
 export const metadata: Metadata = {
-  title: 'Tazkiya Mujahid | Your Favorites Destroyer',
-  description:
-    'Crafted with passion and precision, Tazkiya`s web portfolio reflects the seamless blend of technology and design. 🚀🎨',
+  title: 'Tazkiya Mujahid',
+  description: 'Personal Site of Tazkiya Mujahid',
   icons: {
     icon: '/favicon/favicon.svg',
   },
@@ -37,14 +37,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <UmamiProvider websiteId='2137f71a-6969-41a3-a928-4925dd3dfe12' />
+      </head>
       <body className={cn('', dmSans.variable, instrumentSerif.variable)}>
         <div className='bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] -z-10 fixed inset-0 bg-white bg-[size:6rem_4rem] bg-repeat w-full h-full'></div>
-
         <Cursor />
         <SplashScreen />
         <Navbar />
         <SmoothScrolling>{children}</SmoothScrolling>
-        {/* {children} */}
         <SpeedInsights />
         <div className='noise'></div>
       </body>
